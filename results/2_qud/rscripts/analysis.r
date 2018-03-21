f756b1faf8a749f2591da25c4819d6bb932768cc
thegricean <judithdegen@gmail.com>
2018-03-20 17:10:52 -0700

    updated results

--- results/2_qud/rscripts/analysis.r
+++ results/2_qud/rscripts/analysis.r
@@ -33,10 +33,17 @@ ggplot(d, aes(x=Lang)) +
 
 unique(d$Comments)
 
+d = d %>%
+  mutate(QUD_annotated = as.character(QUD_annotated)) %>%
+  replace_na(list(QUD_annotated="other"))
+d$CorrectQUD = as.factor(ifelse(as.character(d$QUD) == as.character(d$QUD_annotated),"correct","incorrect"))
+table(d$QUD,d$CorrectQUD)
+prop.table(table(d$QUD,d$CorrectQUD),mar=c(1))
+
 # move from wide to long format so there's one data point per row
 dd = d %>%
   select(-Lang,-Education,-Comments,-Age,-Gender,-Lifetime.Approval.Rate) %>%
-  gather(Condition,Response,-workerid,-QUD,-Controlnumber,-ControlQUD) %>%
+  gather(Condition,Response,-workerid,-QUD,-Controlnumber,-Controlqud,-QUD_annotated,-CorrectQUD,-Number_annotated) %>%
   filter(!is.na(Response)) %>%
   mutate(condition = gsub("\\.","--",Condition)) %>%
   droplevels() 
@@ -46,6 +53,7 @@ dd$Scene = sapply(strsplit(as.character(dd$condition),"--"), "[", 2)
 
 head(dd)
 
+# means by qud for all utterances
 means = dd %>%
   group_by(Utterance,Scene,QUD) %>%
   summarize(Mean=mean(Response),CILow=ci.low(Response),CIHigh=ci.high(Response)) %>%
@@ -60,6 +68,21 @@ ggplot(means,aes(x=QUD,y=Mean,fill=Scene)) +
   facet_wrap(~Utterance)
 ggsave("../graphs/allmeans.pdf",width=8,height=6)
 
+# means by qud for all utterances, conditioned on CorrectQUD
+means = dd %>%
+  group_by(Utterance,Scene,QUD,CorrectQUD) %>%
+  summarize(Mean=mean(Response),CILow=ci.low(Response),CIHigh=ci.high(Response)) %>%
+  ungroup() %>%
+  mutate(YMin=Mean-CILow,YMax=Mean+CIHigh)
+
+dodge = position_dodge(.9)
+
+ggplot(means,aes(x=QUD,y=Mean,fill=Scene)) +
+  geom_bar(stat="identity",position=dodge) +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),position=dodge,width=.25) +
+  facet_grid(Utterance~CorrectQUD)
+ggsave("../graphs/allmeans_correctQUD.pdf",width=9,height=12)
+
 # for analysis, look only at "each...some" utterances and exclude floor/ceiling sanity checks
 eachsome = dd %>% 
   filter(Utterance == "Eachsome") %>%
@@ -72,6 +95,27 @@ QUD=recode(QUD,eachany="all-any",anyany="any-any",allall="all-all",anyall="any-a
 
 summary(eachsome)
 
+# who responded TRUE on FALSE trials or FALSE on TRUE trials??
+badfalsesubjects = eachsome %>%
+  filter(Condition == "false") %>%
+  select(workerid,Response) %>%
+  mutate(IncorrectResponse=ifelse(Response == "1","bad subject", "good subject")) %>%
+  filter(IncorrectResponse == "bad subject") %>%
+  select(workerid)
+
+badtruesubjects = eachsome %>%
+  filter(Condition == "true") %>%
+  select(workerid,Response) %>%
+  mutate(IncorrectResponse=ifelse(Response == "0","bad subject", "good subject")) %>%
+  filter(IncorrectResponse == "bad subject") %>%
+  select(workerid)
+
+badsubjects = union(badfalsesubjects,badtruesubjects)
+nrow(badsubjects)
+
+eachsome$IncorrectControlResponse = ifelse(as.character(eachsome$workerid) %in% as.character(badsubjects$workerid), "bad subject","good subject")
+
+#eachsome means
 eachsomemeans = eachsome %>%
   group_by(Condition,QUD) %>%
   mutate(Response = as.numeric(as.character(Response))) %>%
@@ -87,6 +131,82 @@ ggplot(eachsomemeans,aes(x=QUD,y=Mean,fill=Condition)) +
 #  geom_point(data=eachsome,aes(y=as.numeric(as.character(Response)),color=Condition),position=dodge)
 ggsave("../graphs/eachsomemeans.pdf",width=7,height=4)
 
+# eachsome means conditioned on correctness of QUD
+eachsomemeans = eachsome %>%
+  group_by(Condition,QUD,CorrectQUD) %>%
+  mutate(Response = as.numeric(as.character(Response))) %>%
+  summarize(Mean=mean(Response),CILow=ci.low(Response),CIHigh=ci.high(Response)) %>%
+  ungroup() %>%
+  mutate(YMin=Mean-CILow,YMax=Mean+CIHigh)
+dodge = position_dodge(.9)
+
+ggplot(eachsomemeans,aes(x=QUD,y=Mean,fill=Condition)) +
+  geom_bar(stat="identity",position=dodge) +
+  ylab("% TRUE") +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),position=dodge,width=.25) +
+  facet_wrap(~CorrectQUD)
+#  geom_point(data=eachsome,aes(y=as.numeric(as.character(Response)),color=Condition),position=dodge)
+ggsave("../graphs/eachsomemeans_correctQUD.pdf",width=12,height=4)
+
+# eachsome means conditioned on correctly responding FALSE in false condition and TRUE in true condition
+eachsomemeans = eachsome %>%
+  filter(!is.na(IncorrectControlResponse)) %>%
+  group_by(Condition,QUD,IncorrectControlResponse) %>%
+  mutate(Response = as.numeric(as.character(Response))) %>%
+  summarize(Mean=mean(Response),CILow=ci.low(Response),CIHigh=ci.high(Response)) %>%
+  ungroup() %>%
+  mutate(YMin=Mean-CILow,YMax=Mean+CIHigh)
+dodge = position_dodge(.9)
+
+ggplot(eachsomemeans,aes(x=QUD,y=Mean,fill=Condition)) +
+  geom_bar(stat="identity",position=dodge) +
+  ylab("% TRUE") +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),position=dodge,width=.25) +
+  facet_wrap(~IncorrectControlResponse)
+#  geom_point(data=eachsome,aes(y=as.numeric(as.character(Response)),color=Condition),position=dodge)
+ggsave("../graphs/eachsomemeans_incorrectControlResponses.pdf",width=12,height=4)
+
+# eachsome means conditioned on correctly responding FALSE in false condition and TRUE in true condition
+# also conditioned on whether or not they responded correctly to the number of marbles control question
+
+table(eachsome$Number_annotated)
+table(eachsome$IncorrectControlResponse)
+
+eachsomemeans = eachsome %>%
+  filter(!is.na(IncorrectControlResponse)) %>%
+  group_by(Condition,QUD,IncorrectControlResponse,Number_annotated) %>%
+  mutate(Response = as.numeric(as.character(Response))) %>%
+  summarize(Mean=mean(Response),CILow=ci.low(Response),CIHigh=ci.high(Response)) %>%
+  ungroup() %>%
+  mutate(YMin=Mean-CILow,YMax=Mean+CIHigh)
+dodge = position_dodge(.9)
+
+ggplot(eachsomemeans,aes(x=QUD,y=Mean,fill=Condition)) +
+  geom_bar(stat="identity",position=dodge) +
+  ylab("% TRUE") +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),position=dodge,width=.25) +
+  facet_grid(Number_annotated~IncorrectControlResponse)
+#  geom_point(data=eachsome,aes(y=as.numeric(as.character(Response)),color=Condition),position=dodge)
+ggsave("../graphs/eachsomemeans_incorrectControlResponses_number.pdf",width=12,height=7)
+
+# eachsome means conditioned on empirical QUD and whether or not they responded correctly on control trials
+eachsomemeans = eachsome %>%
+  group_by(Condition,QUD_annotated,Number_annotated,IncorrectControlResponse) %>%
+  mutate(Response = as.numeric(as.character(Response))) %>%
+  summarize(Mean=mean(Response),CILow=ci.low(Response),CIHigh=ci.high(Response)) %>%
+  ungroup() %>%
+  mutate(YMin=Mean-CILow,YMax=Mean+CIHigh)
+dodge = position_dodge(.9)
+
+ggplot(eachsomemeans,aes(x=QUD_annotated,y=Mean,fill=Condition)) +
+  geom_bar(stat="identity",position=dodge) +
+  ylab("% TRUE") +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),position=dodge,width=.25) +
+  facet_grid(Number_annotated~IncorrectControlResponse) +
+  theme(axis.text.x=element_text(angle=45, hjust=1,vjust=1))
+#  geom_point(data=eachsome,aes(y=as.numeric(as.character(Response)),color=Condition),position=dodge)
+ggsave("../graphs/eachsomemeans_empiricalQUDandNumber.pdf",width=12,height=8)
+
 toanalyze = eachsome %>% 
   filter(Condition %in% c("weak","literal")) %>% 
   mutate(Condition=as.factor(Condition)) %>%
@@ -95,6 +215,14 @@ toanalyze = eachsome %>%
 contrasts(toanalyze$Condition) = cbind("weak.to.literal"=c(1,0))
 # contrasts(toanalyze$QUD) = cbind("allall.vs.anyany"=c(1,0,0,0),"anyall.vs.anyany"=c(0,1,0,0),"allany.vs.anyany"=c(0,0,0,1))
 
+table(toanalyze$QUD, toanalyze$CorrectQUD, toanalyze$IncorrectControlResponse, toanalyze$Number_annotated)
+
 # Model (with interaction does not currently converge)
 m = glmer(Response ~ Condition + QUD + (1|workerid),family="binomial",data=toanalyze)
+summary(m)
+
+m = glmer(Response ~ Condition + QUD + IncorrectControlResponse + Number_annotated + (1|workerid),family="binomial",data=toanalyze)
+summary(m)
+
+m = glmer(Response ~ Condition + QUD + CorrectQUD + IncorrectControlResponse + Number_annotated + (1|workerid),family="binomial",data=toanalyze)
 summary(m)
